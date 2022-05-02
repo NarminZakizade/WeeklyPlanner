f29b72f26610235434e69d94d007fea039e957ed
NarminZakizade <45127569+NarminZakizade@users.noreply.github.com>
2022-05-02 17:02:49 -0400

    Updates

---
+++ precache-manifest.5eaf9f26c9da4920a6bbe7ce0ab5e151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c028d8007baed015716",
+    "url": "/WeeklyPlanner/static/css/main.a54f43d6.chunk.css"
+  },
+  {
+    "revision": "9c028d8007baed015716",
+    "url": "/WeeklyPlanner/static/js/main.9c028d80.chunk.js"
+  },
+  {
+    "revision": "797a0c6b6aa075c15ac6",
+    "url": "/WeeklyPlanner/static/js/1.797a0c6b.chunk.js"
+  },
+  {
+    "revision": "79ad9aa8be733cf287cb",
+    "url": "/WeeklyPlanner/static/js/runtime~main.79ad9aa8.js"
+  },
+  {
+    "revision": "c1632cfa67f01b6bddfd64e4283bb427",
+    "url": "/WeeklyPlanner/index.html"
+  }
+];
